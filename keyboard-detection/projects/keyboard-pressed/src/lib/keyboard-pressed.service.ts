b96fa6c2452f96e6c0c1a2bc5be8d7fdd4c52790
Juraj Piovar <jpiovar@sk.ibm.com>
2022-03-09 13:55:17 +0100

    keypressed object

--- keyboard-detection/projects/keyboard-pressed/src/lib/keyboard-pressed.service.ts
+++ keyboard-detection/projects/keyboard-pressed/src/lib/keyboard-pressed.service.ts
@@ -1,9 +1,57 @@
-import { Injectable } from '@angular/core';
+import { HostListener, Injectable } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { take } from 'rxjs/operators';
+import * as _ from 'lodash';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KeyboardPressedService {
 
-  constructor() { }
+  // @HostListener('window:keydown', ['$event'])
+  // keyEvent(event: KeyboardEvent) {
+  //   // debugger;
+  //   if (event) {
+  //     console.log('keyCode ', event.keyCode, ' which', event.which, ' key ', event.key);
+  //     //  debugger;
+
+  //   }
+  // }
+
+  activeKeys0: any = {};
+  activeKeys: any = {};
+
+  constructor() {
+
+    fromEvent(window, 'keydown').subscribe((res: any) => {
+      // debugger;
+      this.activeKeys0[res.key] = true;
+      if (res) {
+        if (!_.isEqual(this.activeKeys0, this.activeKeys)) {
+          console.log('keyCode ', res.keyCode, ' which', res.which, ' key ', res.key);
+          //  debugger;
+          this.activeKeys[res.key] = true;
+          this.showActiveKeys();
+        }
+      }
+    });
+
+    fromEvent(window, 'keyup').subscribe((res: any) => {
+      // debugger;
+      delete this.activeKeys0[res.key];
+      if (res) {
+        if (!_.isEqual(this.activeKeys0, this.activeKeys)) {
+          console.log('keyCode ', res.keyCode, ' which', res.which, ' key ', res.key);
+          //  debugger;
+          delete this.activeKeys[res.key];
+          this.showActiveKeys();
+        }
+      }
+    });
+  }
+
+  showActiveKeys() {
+    console.log('showActiveKeys ', this.activeKeys);
+  }
+
 }
--- keyboard-detection/src/app/app.component.ts
+++ keyboard-detection/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KeyboardPressedService } from 'projects/keyboard-pressed/src/lib/keyboard-pressed.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'keyboard-detection';
+
+  constructor(
+    private keyboardPressed: KeyboardPressedService
+  ) {}
 }
